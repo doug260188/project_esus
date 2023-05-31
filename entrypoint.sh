cf1090871842841e284f621a5badfda5ed11b3f6
Rodrigo Silva Rodrigues <rsrodrigues.88@hotmail.com>
2023-05-31 10:00:45 -0400

    v1

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 mkdir -p /var/lock/subsys/e-SUS-AB-PostgreSQL
 service e-SUS-AB-PostgreSQL start
-ENTRYPOINT ["java", "-jar", "eSUS-AB-PEC-5.1.17-Linux64.jar", "-console","--confirm"]
+ENTRYPOINT ["java", "-jar", "eSUS-AB-PEC-5.1.17-Linux64.jar", "-console", "-continue"]
 
 #tail -f /opt/e-SUS/jboss-as-7.2.0.Final/standalone/log/boot.log
