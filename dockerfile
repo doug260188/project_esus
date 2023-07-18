81ac0bba507cfc69c294cb1f52d85a154fe7a109
Rodrigo Silva Rodrigues <rsrodrigues.88@hotmail.com>
2023-07-18 16:30:58 -0400

    1

--- dockerfile
+++ dockerfile
@@ -38,4 +38,4 @@ RUN curl -o /etc/java.conf https://github.com/doug260188/project_esus/blob/maste
 #WORKDIR /app
 
 # Comando para executar a aplicação
-CMD ["java", "-jar", "esus.jar", "-console", "-continue"]
+#CMD ["java", "-jar", "esus.jar", "-console", "-continue"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir -p /var/lock/subsys/e-SUS-AB-PostgreSQL
-service e-SUS-AB-PostgreSQL start
-java -jar eSUS-AB-PEC-5.1.17-Linux64.jar -console -continue
+#mkdir -p /var/lock/subsys/e-SUS-AB-PostgreSQL
+#service e-SUS-AB-PostgreSQL start
+java -jar eSUS-AB-PEC-5.1.17-Linux64.jar -url=localhost:5432/esus -username=postgres -password=esus -console -continue
 
 #tail -f /opt/e-SUS/jboss-as-7.2.0.Final/standalone/log/boot.log
